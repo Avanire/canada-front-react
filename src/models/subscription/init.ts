5a04fb84f70381ed0d5449c152d017c7240b61d1
Avanire <60422131+Avanire@users.noreply.github.com>
2023-01-19 13:12:16 +0600

    added subscribe function

--- src/models/subscription/init.ts
+++ src/models/subscription/init.ts
@@ -5,7 +5,7 @@ import {forward} from "effector";
 
 $subscribePending.on(fetchSubscribeFx.pending, (_, isLoading) => isLoading);
 
-$subscribeSuccess.on(fetchSubscribeFx.doneData, (_, result) => result.status);
+$subscribeSuccess.on(fetchSubscribeFx.doneData, (_, result) => result.message);
 
 forward({
     from: subscribeRequest,
--- src/models/subscription/store.ts
+++ src/models/subscription/store.ts
@@ -2,4 +2,4 @@ import {createStore} from "effector";
 
 export const $subscribePending = createStore<boolean>(false);
 export const $subscribeFailed = createStore<boolean>(false);
-export const $subscribeSuccess = createStore<boolean>(false);
+export const $subscribeSuccess = createStore<string>('');
